c3585126388038794ca67f3c2c831a3821dda12a
venu <gvenu350@gmail.com>
2022-08-23 10:47:55 +0530

    first commit

---
+++ Test01.java
@@ -0,0 +1,6 @@
+public class Test09 {
+
+	public static void main(String[] args) {
+		System.out.println("hello venu");
+	}
+}
---
+++ Test04.java
@@ -0,0 +1,6 @@
+public class Test04 {
+
+	public static void main(String[] args) {
+		System.out.println("Test4");
+	}
+}
